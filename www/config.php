22eb91c4fc0d04c6c96b6b60bc8d92aff9b04131
artoodetoo <i.am@artoodetoo.org>
2019-03-25 23:03:25 +0500

    switch to mysqli extension in auth

--- www/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('CFG_PATH_ROOT', dirname(__FILE__).'/');
-define('CFG_DB_HOSTNAME', 'u476310.mysql.masterhost.ru');
-define('CFG_DB_USERNAME', 'u476310_send');
-define('CFG_DB_PASSWORD', '-ul6o_grEGrI4T');
-define('CFG_DB_DATABASE', 'u476310_send');
-define('CFG_HTTP_ROOT', 'http://www.send-test.olympic-sport.ru');
-?>
--- www/inc/auth.php
+++ www/inc/auth.php
@@ -10,14 +10,14 @@ if(!isset($_SESSION['dev'])){
 
 if(isset($_POST["login"])&&isset($_POST["password"])&&!empty($_POST["login"])&&!empty($_POST["password"]))
 {
-	mysql_connect(CFG_DB_HOSTNAME,CFG_DB_USERNAME,CFG_DB_PASSWORD);
-	mysql_select_db(CFG_DB_DATABASE);
-	$res=mysql_query("SELECT COUNT(*) FROM `mail_user` WHERE Name='".mysql_escape_string($_POST["login"])."' AND Password='".mysql_escape_string($_POST["password"])."'");
-	$c=mysql_result($res,0);
-	if($c>0)
+	$db = mysqli_connect(CFG_DB_HOSTNAME,CFG_DB_USERNAME,CFG_DB_PASSWORD);
+	mysqli_select_db($db, CFG_DB_DATABASE);
+	$res=mysqli_query($db, "SELECT COUNT(*) FROM `mail_user` WHERE Name='".mysqli_escape_string($db, $_POST["login"])."' AND Password='".mysqli_escape_string($db, $_POST["password"])."'");
+	$c=mysqli_fetch_row($res);
+	if($c && $c[0]>0)
 	{
 		$_SESSION["login"]=$_POST["login"];
 	}
-	mysql_close();
+	mysqli_close($db);
 }
 ?>
